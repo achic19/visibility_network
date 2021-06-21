fb4bbe5e3184dfe5b13ebcf9667bfb5cbe9310b5
achic19 <38861334+achic19@users.noreply.github.com>
2021-06-21 09:31:06 +0300

    Comments

--- test/same_area/PolygonPoint.py
+++ test/same_area/PolygonPoint.py
@@ -1,8 +1,11 @@
 from shapely.geometry import Point
 
 
-class Slope:
+class LineParameters:
     def __init__(self, p1: Point, p2: Point):
+        """
+        It creates slope object using two points coordinates (:param p1 and :param p2)
+        """
         self.slope = (p2.y - p1.y) / (p2.x - p1.x)
         self.intersect = p1.y - p1.x * self.slope
 
@@ -12,12 +15,17 @@ class Slope:
 
 class PolygonPoint:
     def __init__(self, pnt):
+        """
+        The PolygonPoint class object contains the current point (:param pnt)
+        and the lines connecting it to the adjacent points
+        in  the  polygon ( another two PolygonPoint point and line parameters)
+        """
         self.pnt = pnt
         self.nxt = None
         self.pre = None
 
-        self.nxt_slp = None
-        self.pre_slp = None
+        self.nxt_line_params = None
+        self.pre_line_params = None
 
     def __repr__(self):
         if self.nxt is not None:
@@ -31,12 +39,17 @@ if __name__ == '__main__':
     my_polygon = [Point(7, 12), Point(4, 12), Point(7, 3), Point(12, 8), Point(2, 2), Point(0, 8), Point(7, 12)]
     print(my_polygon)
     new_list = []
+    # Create two PolygonPoint objects from the the first two Points in the polygon
     fst_pnt = PolygonPoint(my_polygon[0])
     nxt_pnt = PolygonPoint(my_polygon[1])
+    # update the next point of the first PolygonPoint object and put it the new database
     fst_pnt.nxt = nxt_pnt
     new_list.append(fst_pnt)
     pre_pnt = fst_pnt
     for i in range(1, len(my_polygon) - 2):
+        # this loop creates new PolygonPoint object (the next index) and update all rest
+        # points of the current  PolygonPoint object (the current index)
+        # than put it into the database and update the temp variables for the next loop
         new_pnt = PolygonPoint(my_polygon[i + 1])
         nxt_pnt.nxt = new_pnt
         nxt_pnt.pre = pre_pnt
@@ -44,20 +57,23 @@ if __name__ == '__main__':
         pre_pnt = nxt_pnt
         nxt_pnt = new_pnt
 
+    # operation for the last point in the polygon
     nxt_pnt.pre = pre_pnt
     nxt_pnt.nxt = fst_pnt
     new_list.append(nxt_pnt)
     fst_pnt.pre = nxt_pnt
     print(new_list)
+
     for temp_pnt in new_list:
-        if temp_pnt.pre_slp is None:
-            temp_pnt.pre_slp = Slope(temp_pnt.pnt, temp_pnt.pre.pnt)
-            temp_pnt.pre.nxt_slp = temp_pnt.pre_slp
-        if temp_pnt.nxt_slp is None:
-            temp_pnt.nxt_slp = Slope(temp_pnt.pnt, temp_pnt.nxt.pnt)
-            temp_pnt.nxt.pre_slp = temp_pnt.nxt_slp
+        # Update the line parameters for the current point as well as the connected points
+        if temp_pnt.pre_line_params is None:
+            temp_pnt.pre_line_params = LineParameters(temp_pnt.pnt, temp_pnt.pre.pnt)
+            temp_pnt.pre.nxt_line_params = temp_pnt.pre_line_params
+        if temp_pnt.nxt_line_params is None:
+            temp_pnt.nxt_line_params = LineParameters(temp_pnt.pnt, temp_pnt.nxt.pnt)
+            temp_pnt.nxt.pre_line_params = temp_pnt.nxt_line_params
     for temp_pnt in new_list:
-        print(temp_pnt.pre_slp)
-        print(temp_pnt.nxt_slp)
+        print(temp_pnt.pre_line_params)
+        print(temp_pnt.nxt_line_params)
         print(temp_pnt)
 
